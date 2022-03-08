85603dd07f917d8d12a82b963bd51090f1106b16
Stephen Gomez-Fox <stephen@gomezfox.net>
2022-03-07 17:33:10 -0800

    Implements full token_store functionality

--- src/tools.py
+++ src/tools.py
@@ -1,7 +1,4 @@
-import json
 import re
-import jwt
-from datetime import datetime, timedelta
 from flask import current_app, jsonify, make_response
 
 
@@ -38,36 +35,6 @@ def valid_password(password):
     # returns true if password is at least 4 characters
     pw = str(password)
     return len(pw) >= 4
-
-
-def decode_auth_token(auth_token):
-    try:
-        jwt_payload = jwt.decode(
-            auth_token,
-            current_app.config.get('SECRET_KEY')
-        )
-        return jwt_payload['sub']
-    except jwt.ExpiredSignatureError:
-        return json.dumps({'error': 'Signature expired. Please log in again.'})
-    except jwt.InvalidTokenError:
-        return json.dumps({'error': 'Invalid token. Please log in again.'})
-
-
-def encode_auth_token(user_name):
-    try:
-        jwt_header = {}
-        jwt_payload = {
-            'exp': datetime.utcnow() + timedelta(days=1),
-            'iat': datetime.utcnow(),
-            'sub': user_name
-        }
-        return jwt.encode(
-            jwt_payload,
-            current_app.config.get('SECRET_KEY'),
-            algorithm='HS256'
-        )
-    except Exception as e:
-        return e
 
 
 def error_response(code=None, message=None, http_code=500):
