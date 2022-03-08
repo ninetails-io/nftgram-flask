c08b890c83bb3b14fbad0d3266e53e79fbb68e6c
Stephen Gomez-Fox <stephen@gomezfox.net>
2022-03-07 17:56:30 -0800

    Fixes error in user entity from_dict

--- entities/user_entity.py
+++ entities/user_entity.py
@@ -21,12 +21,12 @@ class User:
             "date_joined": self.date_joined
         }
 
-    def from_dict(self, dictionary):
-        if not (type(dictionary) is dict):
+    def from_dict(self, dic):
+        if not (type(dic) is dict):
             raise "Requires dict as input"
         if 'user_id' in dic and 'password' in dic and 'date_joined' in dic:
             self.user_id, self.password, self.date_joined \
-                = user_id, password, date_joined
+                = dic['user_id'], dic['password'], dic['date_joined']
         else:
             raise "Dictionary in UserEntity: missing required fields "
 
